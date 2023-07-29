3f35e1ec08565890842c2b42bbff985ee1a93597
Ben Cooper <ben.f.cooper@gmail.com>
2023-07-29 10:19:57 +0100

    fix: :card_file_box: KAN-14 Add back models

--- detty-december/src/app/business/my-events/page.tsx
+++ detty-december/src/app/business/my-events/page.tsx
@@ -1,38 +1,50 @@
-'use client'
-import {useEffect, useState} from "react";
-import AppBar from "../../../../components/AppBar";
-import TopAppBar from "../../../../components/AppBar";
-import { usePathname, useSearchParams } from 'next/navigation'
+'use client';
+import {useEffect, useState} from 'react';
+import AppBar from '../../../../components/AppBar';
+import TopAppBar from '../../../../components/AppBar';
+import { usePathname, useSearchParams } from 'next/navigation';
+import {v4 as uuidv4} from 'uuid';
 
 
 export default  function Home() {
-    const pathname = usePathname()
-    const parts = pathname.split('/');
-    const url_keyword = parts.pop();
-    const [data, setData] = useState();
-    const fetchData = async () => {
-        try {
-            let requestData = {"userId" : localStorage.getItem('usedId')}
-            const response = await fetch('http://localhost:8000/api/v1/events/', {
-                method: "POST", // *GET, POST, PUT, DELETE, etc.
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                credentials: 'include',
-                body: JSON.stringify(requestData),
-            });
-            setData(await response.json());
-        } catch (error) {
-            console.error(error)
-        }
-    };
-    fetchData().then(()=>{console.log(data)});
-    return (
-        <div>
-           <TopAppBar
-               location={url_keyword}
-           />
-        </div>
-    )
+  const pathname = usePathname();
+  const parts = pathname.split('/');
+  const url_keyword = parts.pop();
+  const [data, setData] = useState();
+  const fetchData = async () => {
+    try {
+      const userId = '01a45099-e08d-49a4-a06c-315f0bf54c63';
+      const userResponse = await fetch('http://localhost:8000/api/v1/business/users/',{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        credentials: 'include',
+        body: JSON.stringify({'userId': userId})
+      }).then((data)=>{console.log(data);});
+      localStorage.setItem('userId', userId);
+      const ownerId = localStorage.getItem('userId');
+      const eventsResponse = await fetch(`http://localhost:8000/api/v1/business/events?ownerid=${ownerId}`,{
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        credentials: 'include',
+      });
+      setData(await eventsResponse.json());
+    }
+    catch (error) {
+      console.error(error);
+    }
+  };
+  fetchData().then(()=>{console.log(data);});
+  return (
+    <div>
+      <TopAppBar
+        location={url_keyword}
+      />
+    </div>
+  );
 }
